1481f20d39d0ab1f9825b16f6822042366a79680
santhoshgurram666 <santhoshgurram2006@gmail.com>
2025-11-15 06:59:02 +0530

    Create re.js

---
+++ re.js
@@ -0,0 +1,12 @@
+import { createRoot } from 'react-dom/client' 
+createRoot(document.getElementById('root')).render(
+ <p>Welcome!</p>
+)
+Index.html 
+<!doctype html>
+<html lang="en"> 
+<body>
+ <div id="root"></div> 
+ <script type="module" src="/src/main.jsx"></script> 
+</body> 
+</html>
